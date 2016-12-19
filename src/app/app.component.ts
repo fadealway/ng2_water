755b8a342bfc45f79d125d9aa898599cbeaab5c9
fadealway <ittipatpt@gmail.com>
2016-12-19 16:52:27 +0700

    add data mockup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,23 @@ export class AppComponent {
     get_data_all():any {
       this.water.get_data_daily()
       .subscribe(ut_daily => { 
-
+        // console.log("ut_daily",ut_daily)
+        // {
+        //   "Table": [
+        //     {
+        //       "ServerDT": "2016-12-19T16:23:11.16",
+        //       "ResultType": "DAILY",
+        //       "TagID": "TOT_RO_WATER",
+        //       "mySUM": 37796.385
+        //     },
+        //     {
+        //       "ServerDT": "2016-12-19T16:23:11.16",
+        //       "ResultType": "DAILY",
+        //       "TagID": "TOT_SCHOTDI",
+        //       "mySUM": 38985.385
+        //     }
+        //   ]
+        // }
         this.raw_water.name = "Raw/City water";
         this.raw_water.daily = ut_daily[0].mySUM;
 
@@ -48,6 +64,23 @@ export class AppComponent {
 
       this.water.get_data_monthly()
       .subscribe(ut_monthly => { 
+//  console.log("ut_monthly",ut_monthly)
+// {
+//   "Table": [
+//     {
+//       "ServerDT": "2016-12-19T16:23:19.903",
+//       "ResultType": "MONTHLY",
+//       "TagID": "TOT_RO_WATER",
+//       "mySUM": 38671.01
+//     },
+//     {
+//       "ServerDT": "2016-12-19T16:23:19.903",
+//       "ResultType": "MONTHLY",
+//       "TagID": "TOT_SCHOTDI",
+//       "mySUM": 46904.39
+//     }
+//   ]
+// }
 
         this.raw_water.monthly = ut_monthly[0].mySUM;
         this.MMF_backwash.monthly = ut_monthly[1].mySUM;
