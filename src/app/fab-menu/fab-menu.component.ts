2745841d63310a8cfa5efbff684fea45d9f60d1c
fadealway <ittipatpt@gmail.com>
2016-12-21 14:47:11 +0700

    add fab menu component

---
+++ src/app/fab-menu/fab-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fab-menu',
+  templateUrl: './fab-menu.component.html',
+  styleUrls: ['./fab-menu.component.scss']
+})
+export class FabMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
