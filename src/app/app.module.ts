2e589a5a764d06b96a42add1660b5cc033ea4878
fadealway <ittipatpt@gmail.com>
2016-12-21 14:48:03 +0700

    add fabmenu,fabmenuscss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { CardWaterComponent } from './card-water/card-water.component';
 import { CardWaterHeaderComponent } from './card-water-header/card-water-header.component';
 import { CardWaterTableComponent } from './card-water-table/card-water-table.component';
 import { WaterStatusDirective } from './water-status.directive';
+import { FabMenuComponent } from './fab-menu/fab-menu.component';
+import { FabMenuScssComponent } from './fab-menu-scss/fab-menu-scss.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { WaterStatusDirective } from './water-status.directive';
     CardWaterComponent,
     CardWaterHeaderComponent,
     CardWaterTableComponent,
-    WaterStatusDirective
+    WaterStatusDirective,
+    FabMenuComponent,
+    FabMenuScssComponent
   ],
   imports: [
     BrowserModule,
